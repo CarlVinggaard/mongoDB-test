db21de59f58fee6e8b4947f14a2a833f8db44ac0
Carl Vinggaard Kjeldsen <carl@kvalifik.dk>
2020-06-07 20:48:51 +0200

    Initial commit from local workspace

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import flask_pymongo
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb+srv://carlvinggaard:18M0n90d603@cluster0-lqr5z.mongodb.net/task_manager?retryWrites=true&w=majority"
+mongo = PyMongo(app)
+
+
+@app.route("/")
+def home_page():
+    post = {"is_urgent": True, "task_name": "Pick up the kids"}
+    mongo.db.tasks.insert_one(post)
+    return "Post inserted." 
